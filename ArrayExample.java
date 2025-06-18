b28e9c2c8b3ce99d03b27fe7f1442bdf5e66884e
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-06-18 08:58:01 +0200

    New file

---
+++ ArrayExample.java
@@ -0,0 +1,9 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numeri = {10, 20, 30, 40, 50};
+
+        for (int num : numeri) {
+            System.out.println(num);
+        }
+    }
+}
