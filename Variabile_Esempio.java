b0a22144899233f9cb1230c87ecdb672075ffc51
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-06-27 18:09:37 +0200

    New file

---
+++ Variabile_Esempio.java
@@ -0,0 +1,12 @@
+public class Variabile_Esempio {
+    public static void main(String[] args) {
+        int eta = 31;
+        double altezza = 1.72;
+        char inizialeNome = 'V';
+        boolean isMaschio = true;
+        String nome = "Valerio";
+
+        System.out.println("Ciao! Io mi chiamo " + nome + ", ho " + eta + "anni, il mio nome inizia per " + inizialeNome
+                + " e sono alto " + altezza + "m.");
+    }
+}
