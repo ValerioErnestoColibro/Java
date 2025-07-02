31f51805821be7f0ef77261bbf4ca8552307e20a
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-07-02 14:36:58 +0200

    New file

---
+++ Tipi_dato.java
@@ -0,0 +1,14 @@
+public class Tipi_dato {
+   public static void main(String[] args) {
+      int numero = 10;
+      double decimale = 10.5;
+      char carattere = 'A';
+      String testo = "Ciao";
+      boolean logico = true;
+      System.out.println("Numero: " + numero);
+      System.out.println("Decimale: " + decimale);
+      System.out.println("Carattere: " + carattere);
+      System.out.println("Testo: " + testo);
+      System.out.println("Logico: " + logico);
+   } 
+}
