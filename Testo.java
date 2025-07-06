f2d4a76b259ac93cd329e3692e0ab37c6b85cfed
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-07-06 22:07:59 +0200

    New file

---
+++ Testo.java
@@ -0,0 +1,5 @@
+public class Testo { // Classe pubblica Testo
+    public static void main(String[] args) { // Metodo pubblico statico main
+        System.out.println("Questo è un testo di prova, lo vedi?"); // Stampa il testo
+    }
+}
