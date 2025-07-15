0de7e9fa605edfbe6c84e80defc5eda77fb1dd50
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-07-15 12:01:17 +0200

    New file

---
+++ Stampa_variabili.java
@@ -0,0 +1,8 @@
+public class Stampa_variabili {
+    public static void main(String[] args) {
+        int età = 31;
+        double altezza = 1.75;
+        System.out.println("La mia età è di " + età + " anni");
+        System.out.println("La mia altezza è di " + altezza + "m");
+}
+}
