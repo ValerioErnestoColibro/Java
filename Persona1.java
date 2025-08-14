bc9814b1551622213c1d311eef53ceff624efeab
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-08-15 00:40:42 +0200

    New file

---
+++ Persona1.java
@@ -0,0 +1,8 @@
+public class Persona1 {
+    public static void saluta() {
+        System.out.println("Ciao, sono una persona!");
+    }
+    public static void main(String[] args) {
+        saluta();
+    }
+}
