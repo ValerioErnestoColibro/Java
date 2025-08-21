7b507fed8cd6ef9864d52eb20c7746ef1a45e108
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-08-21 18:47:58 +0200

    New file

---
+++ Operazioni.java
@@ -0,0 +1,9 @@
+public class Operazioni {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("La somma di " + a + " + " + b + " è uguale a " + (a + b));
+        System.out.println("La differenza di " + a + " - " + b + " è uguale a " + (a - b));
+        System.out.println("Il prodotto di " + a + " * " + b + " è uguale a " + (a * b));
+    }
+}
