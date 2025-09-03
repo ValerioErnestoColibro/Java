261e7798bf228959f3a8decdb77b4ce5f143c299
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-09-03 11:55:57 +0200

    New file

---
+++ Metodo_Area_Rettangolo.java
@@ -0,0 +1,9 @@
+public class Metodo_Area_Rettangolo {
+    public static int calcolaArea(int base, int altezza) {
+        return base * altezza;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Area del rettangolo: " + calcolaArea(10, 5));
+    }
+}
