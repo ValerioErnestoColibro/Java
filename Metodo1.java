4d17195c0ebf88bee0a6b6538965bc676f2cd16c
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-09-11 14:05:27 +0200

    New file

---
+++ Metodo1.java
@@ -0,0 +1,10 @@
+public class Metodo1 {
+    // Metodo che stampa un messaggio
+    public static void stampaMessaggio() {
+        System.out.println("Primo metodo con Java!");
+    }
+
+    public static void main(String[] args) {
+        stampaMessaggio(); // Chiamata al metodo
+    }
+}
