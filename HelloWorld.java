32d953a1c17c709a8d5f537ad1fe6b5ad4c92455
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-10-05 13:53:12 +0200

    New file

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { // Classe pubblica HelloWorld
+    public static void main(String[] args) { // Metodo pubblico statico main
+        System.out.println("Ciao, mondo!"); // Stampa "Ciao, mondo!"
+    }
+}
