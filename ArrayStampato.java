dcadc382e3e67e86b7aebff2b3cadbccf39317a1
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-10-13 20:26:48 +0200

    New file

---
+++ ArrayStampato.java
@@ -0,0 +1,11 @@
+public class ArrayStampato {
+    public static void main (String[] args) {
+        // Creazione di un array di numeri
+        int[] numeri = {23, 44, 1, 64, 97};
+
+        // Ciclo for per stampare ogni elemento
+        for (int i = 0; i < numeri.length; i++) {
+            System.out.println("Elemento in posizione " + i + ": " + numeri[i]);
+        }
+    }
+}
