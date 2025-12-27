bf8183b60fc28c6ae54b7f831a29717911f0c3dc
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-12-27 15:10:32 +0100

    New file

---
+++ Controllo_flusso.java
@@ -0,0 +1,10 @@
+public class Controllo_flusso {
+    public static void main(String[] args) {
+        int numero = 10;
+        if (numero > 0) {
+            System.out.println("Il numero è positivo");
+        } else {
+            System.out.println("Il numero è negativo");
+        }
+    }
+}
