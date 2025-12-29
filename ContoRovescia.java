89c465347b9762e4d562d5cb5b5e1b5391c6c873
ValerioErnestoColibro <166521996+ValerioErnestoColibro@users.noreply.github.com>
2025-12-29 13:19:47 +0100

    New file

---
+++ ContoRovescia.java
@@ -0,0 +1,11 @@
+public class ContoRovescia {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        while (numero > 0) {
+            System.out.println("Meno " + numero);
+            numero--; // Decrementa il numero di 1 ad ogni passo
+        }
+        System.out.println("BUON ANNO!");
+    }
+}
